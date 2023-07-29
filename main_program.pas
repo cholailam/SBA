cb1d03e489dce8a0cb1d95d52baea0acd3e55589
Cho Lai Lam <107162584+cholailam@users.noreply.github.com>
2023-07-30 01:37:42 +0800

    Update main_program.pas

--- main_program.pas
+++ main_program.pas
@@ -1,14 +1,21 @@
 program project1;
 
 uses
-  split_string, {a unit with function: to_array, get_file}
+  general, {a unit with function: to_array, get_file}
   SysUtils, {to use TStringArray}
-  Classes; {to use TStringList}
-
+  Classes,  {to use TStringList}
+  begin_with_pronoun;
 var
-
+  link: string;
+  paragraph: TStringList;
+  count: integer;
 
 begin
-  writeln((get_file('text.txt'))[4]);
+  write('Input the link of text file: ');
+  readln(link);
+  paragraph := get_file(link);
+  writeln(paragraph[0]);
+  count :=  begin_pronoun(paragraph[0]);
+  writeln(IntToStr(count));
   readln();
 end.
