8159c4793c71c5300f2c2fb82e8b8af54288854c
Cho Lai Lam <107162584+cholailam@users.noreply.github.com>
2023-08-08 21:50:23 +0800

    Update begin_with_pronoun.pas

--- begin_with_pronoun.pas
+++ begin_with_pronoun.pas
@@ -3,25 +3,27 @@ unit begin_with_pronoun;
 interface
 
 uses
-  Classes, SysUtils, general;
+  Classes, SysUtils, general, StrUtils;
 
-function begin_pronoun(paragraph: string): integer;
+function begin_pronoun(paragraph: ansistring): integer;
 
 var
   sentences: TStringArray;
-  line: string;
+  line: ansistring;
   words: TStringArray;
   count: integer;
+  pronouns: array of String = ('He', 'She', 'It', 'I', 'We', 'You', 'They');
 
 implementation
 
-function begin_pronoun(paragraph: string): integer;
+function begin_pronoun(paragraph: ansistring): integer;
 begin
+  count := 0;
   sentences := to_array(paragraph, '.');
   for line in sentences do
   begin
-    words := to_array(line, ' ');
-    if (words[0] = 'He') then
+    words := to_array(Trim(line), ' ');
+    if (words[0] in pronouns)then
     begin
       count := count + 1;
     end;
