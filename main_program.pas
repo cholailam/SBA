63aab5bac3debbb62a4de2fa47c40fbfc2659b6e
Cho Lai Lam <107162584+cholailam@users.noreply.github.com>
2023-08-08 21:51:44 +0800

    Update main_program.pas

--- main_program.pas
+++ main_program.pas
@@ -1,21 +1,23 @@
-program project1;
+program english_writing_analyser;
 
 uses
   general, {a unit with function: to_array, get_file}
   SysUtils, {to use TStringArray}
   Classes,  {to use TStringList}
-  begin_with_pronoun;
+  begin_with_pronoun,
+  StrUtils;
+
 var
   link: string;
   paragraph: TStringList;
   count: integer;
 
 begin
-  write('Input the link of text file: ');
+  write('Input the name of text file: ');
   readln(link);
   paragraph := get_file(link);
   writeln(paragraph[0]);
   count :=  begin_pronoun(paragraph[0]);
-  writeln(IntToStr(count));
+  writeln(count);
   readln();
 end.
