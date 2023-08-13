b701c8a59601ea6e05c116baf63d925b26600991
Cho Lai Lam <107162584+cholailam@users.noreply.github.com>
2023-08-14 01:28:01 +0800

    Update english_writing_analyser.pas

--- english_writing_analyser.pas
+++ english_writing_analyser.pas
@@ -4,18 +4,20 @@ uses
   general, {a unit with function: to_array, get_file}
   SysUtils, {to use TStringArray}
   Classes,  {to use TStringList}
-  wordings;
+  wordings,
+  Generics.Collections;
 
 var
   link, analyser: string;
   paragraph: TStringList;
   each_para: ansistring;
+  all_sen: string;
 
 begin
   repeat
-    write('Input the name of text file: ');
+    write('Input the name of text file (eg. if name is text.txt, enter text): ');
     readln(link);
-    paragraph := get_file(link);
+    paragraph := get_file(link+'.txt');
     if paragraph = nil then
       begin
         writeln('Cannot find file');
@@ -33,17 +35,26 @@ begin
     writeln();
     writeln('=================================');
 
+    all_sen := pure_sen(paragraph);
     if analyser = '1' then
       begin
+        writeln('Frequency of');
+        writeln(frequency(all_sen)['is']);
+        writeln();
         writeln('Number of sentences start with pronoun');
         for each_para in paragraph do
           begin
             writeln('paragraph ', paragraph.IndexOf(each_para)+1, ': ', begin_pronoun(each_para));
           end;
+      end
+    else if analyser = '2' then
+      begin
+        writeln('Under construction');
       end;
     writeln('=================================');
-    until analyser = '';
-    writeln('Goodbye');
+  until analyser = '';
+
+  writeln('Goodbye');
 
   readln();
 end.
