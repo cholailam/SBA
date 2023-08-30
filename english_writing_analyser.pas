3d0302d1f7614deb54810ab2f07e7bcf0cca9b7b
Cho Lai Lam <107162584+cholailam@users.noreply.github.com>
2023-08-30 23:06:35 +0800

    Update english_writing_analyser.pas

--- english_writing_analyser.pas
+++ english_writing_analyser.pas
@@ -4,7 +4,8 @@ uses
   general, {a unit with function: to_array, get_file}
   SysUtils, {to use TStringArray}
   Classes,  {to use TStringList}
-  wordings,
+  StrUtils, {to use PosEx}
+  wordings, readability,
   Generics.Collections;
 
 var
@@ -12,6 +13,8 @@ var
   paragraph: TStringList;
   each_para: ansistring;
   all_sen: string;
+  connectives: array of string = ('addition', 'result', 'contrast', 'summarize');
+  cata: string;
 
 begin
   repeat
@@ -35,22 +38,30 @@ begin
     writeln();
     writeln('=================================');
 
-    all_sen := pure_sen(paragraph);
+    all_sen := pure_sen(paragraph); {convert list of paragraphs to string of sentences}
     if analyser = '1' then
       begin
         writeln('Frequency of');
         writeln(frequency(all_sen)['is']);
         writeln();
+
         writeln('Number of sentences start with pronoun');
         for each_para in paragraph do
           begin
             writeln('paragraph ', paragraph.IndexOf(each_para)+1, ': ', begin_pronoun(each_para));
           end;
       end
+
     else if analyser = '2' then
       begin
         writeln('Under construction');
+        for cata in connectives do
+        begin
+          write('Number of ', cata, ' connectives: ');
+          writeln(num_connectives(all_sen, cata));
+        end;
       end;
+
     writeln('=================================');
   until analyser = '';
 
