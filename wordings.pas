cb2e41b215faed99ced8a748fffc24912db001aa
Cho Lai Lam <107162584+cholailam@users.noreply.github.com>
2023-09-01 17:20:45 +0800

    Update wordings.pas

--- wordings.pas
+++ wordings.pas
@@ -1,113 +1,52 @@
-unit readability;
+unit wordings;
 
 interface
 
 uses
-  Classes, SysUtils, general, strUtils;
+  Classes, SysUtils, general, StrUtils, math, Generics.Collections;
 
-var
-  con_addition: array of string = ('also', 'besides', 'furthermore', 'too', 'moreover', 'in addition', 'then', 'of equal importance', 'equally important', 'another', 'additionally', 'apart from this', 'as well as');
-  con_result: array of string = ('as a result', 'hence', 'accordingly', 'as a consequence', 'consequently', 'thus', 'since', 'therefore', 'for this reason', 'because of this');
-  con_contrast: array of string = ('in contrast', 'conversely', 'however', 'still', 'nevertheless', 'nonetheless', 'yet', 'on the other hand', 'on the contrary', 'in spite of', 'actually', 'in fact', 'despite', 'although', 'alternatively', 'even so', 'instead', 'contrary to', 'notwithstanding', 'on the other hand', 'rather', 'whereas');
-  con_summarize: array of string = ('in summary', 'to sum up', 'to repeat', 'briefly', 'in short', 'finally', 'on the whole', 'therefore', 'as I have said', 'in conclusion', 'as you can see');
+function find_max_fre(fre_of_vocab: TIntegerArray): integer;
+function begin_pronoun(paragraph: ansistring): integer;
 
-function count_substring(sub_str, whole_str: string): integer;
-function num_connectives(sentences: ansistring; cata: string): integer;
-function give_connectives(cata: string): string;
 
 implementation
 
-function count_substring(sub_str, whole_str: string): integer;
+function begin_pronoun(paragraph: ansistring): integer;
 var
-  start: integer;
-  point: integer;
-  num_sub: integer;
+  sentences, words: TStringArray;
+  line: ansistring;
+  count: integer;
+  pronouns: array of String = ('He', 'She', 'It', 'I', 'We', 'You', 'They');
 begin
-  num_sub := 0;
-  start := 1;
-  point := PosEx(sub_str, whole_str, start);
-  while (point > 0) do
-  begin
-    start := point + 1;
-    num_sub := num_sub + 1;
-    point := PosEx(sub_str, LowerCase(whole_str), start);
-  end;
-  count_substring := num_sub;
-end;
-
-
-
-function num_connectives(sentences: ansistring; cata: string): integer;
-var
-  line: TStringArray;
-  words, connect: string;
-  count, all_count: integer;
-
-begin
-  all_count := 0;
   count := 0;
-  line := to_array(sentences,'.');
-  for words in line do
+  sentences := to_array(paragraph, '.');
+  for line in sentences do
   begin
-    if (cata = 'addition') then
+    words := to_array(Trim(line), ' ');
+    if (words[0] in pronouns)then
     begin
-      for connect in con_addition do
-      begin
-        count := count_substring(connect, words);
-        all_count := all_count + count;
-      end;
-    end
-
-    else if (cata = 'result') then
-    begin
-      for connect in con_result do
-      begin
-        count := count_substring(connect, words);
-        all_count := count + all_count;
-      end;
-    end
-
-    else if (cata = 'contrast') then
-    begin
-      for connect in con_contrast do
-      begin
-        count := count_substring(connect, words);
-        all_count := count + all_count;
-      end;
-    end
-
-    else if (cata = 'summarize') then
-    begin
-      for connect in con_summarize do
-      begin
-        count := count_substring(connect, words);
-        all_count := count + all_count;
-      end;
+      count := count + 1;
     end;
-
   end;
-  num_connectives := all_count;
+  begin_pronoun := count;
 end;
 
 
-function give_connectives(cata: string): string;
+{return the index of max value}
+function find_max_fre(fre_of_vocab: TIntegerArray): integer;
 var
-  rand_con: string;
+  max_fre: integer;
+  i: integer;
 begin
-  Randomize;
-  if (cata = 'addition') then
-    rand_con := con_addition[random(length(con_addition))]
-
-  else if (cata = 'result') then
-    rand_con := con_result[random(length(con_result))]
-
-  else if (cata = 'contrast') then
-    rand_con := con_contrast[random(length(con_contrast))]
-
-  else if (cata = 'summarize') then
-    rand_con := con_summarize[random(length(con_summarize))];
-  give_connectives := rand_con;
+  max_fre := maxvalue(fre_of_vocab);
+  for i := 0 to (length(fre_of_vocab)-1) do
+  begin
+    if (fre_of_vocab[i] = max_fre) then
+    find_max_fre := i;
+  end;
+  find_max_fre := -1;
 end;
+
+
 
 end.
-
