86a6dbd2c689a671cf60fdf47cc8f2579e523a4a
Cho Lai Lam <107162584+cholailam@users.noreply.github.com>
2023-09-02 17:43:59 +0800

    Update wordings.pas

--- wordings.pas
+++ wordings.pas
@@ -6,30 +6,11 @@ uses
   Classes, SysUtils, general, StrUtils, math, Generics.Collections;
 
 function find_max_fre(fre_of_vocab: TIntegerArray): integer;
+function freq_of_keyword(keyword: string; all_words: specialize TList<string>): integer;
 function begin_pronoun(paragraph: ansistring): integer;
 
 
 implementation
-
-function begin_pronoun(paragraph: ansistring): integer;
-var
-  sentences, words: TStringArray;
-  line: ansistring;
-  count: integer;
-  pronouns: array of String = ('He', 'She', 'It', 'I', 'We', 'You', 'They');
-begin
-  count := 0;
-  sentences := to_array(paragraph, '.');
-  for line in sentences do
-  begin
-    words := to_array(Trim(line), ' ');
-    if (words[0] in pronouns)then
-    begin
-      count := count + 1;
-    end;
-  end;
-  begin_pronoun := count;
-end;
 
 
 {return the index of max value}
@@ -46,6 +27,43 @@ begin
   end;
   find_max_fre := -1;
 end;
+
+
+function freq_of_keyword(keyword: string; all_words: specialize TList<string>): integer;
+var
+  count: integer;
+  word: string;
+begin
+  for word in all_words do
+  begin
+    if (word = keyword) then
+      count += 1;
+  end;
+  freq_of_keyword := count;
+end;
+
+
+function begin_pronoun(paragraph: ansistring): integer;
+const
+  pronouns: array[0..6] of String = ('He', 'She', 'It', 'I', 'We', 'You', 'They');
+var
+  sentences, words: TStringArray;
+  line: ansistring;
+  count: integer;
+begin
+  count := 0;
+  sentences := to_array(paragraph, '.');
+  for line in sentences do
+  begin
+    words := to_array(Trim(line), ' ');
+    if (words[0] in pronouns)then
+    begin
+      count := count + 1;
+    end;
+  end;
+  begin_pronoun := count;
+end;
+
 
 
 
